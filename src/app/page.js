9c71cf92ecd44a17d8de83f8cbbb49f4a000f988
Kazi Efazul Karim <efazul.fayad1717@gmail.com>
2024-02-13 23:04:34 +0600

    Add Datadog browser RUM monitoring

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 "use client";
-
+import { useRouter } from "next/router";
 import { useState } from "react";
-import Script from "next/script";
 import PageBox from "@/components/common/PageBox";
 import NavBar from "@/components/navbar/NavBar";
 import HomeSection1 from "@/components/home/Section1";
@@ -10,12 +9,41 @@ import HomeSection4 from "@/components/home/Section4";
 import HomeSection5 from "@/components/home/Section5";
 import HomeSection6 from "@/components/home/Section6";
 import Head from "next/head";
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+  applicationId: 'a1cc279e-cc0c-41cb-ae9b-769883f80d37',
+  clientToken: 'pub35d708aad60885f0e22e4c7c634c78f2',
+  site: 'us5.datadoghq.com',
+  service: 'efazul_portfolio',
+  env: 'production',
+  sessionSampleRate: 100,
+  sessionReplaySampleRate: 20,
+  trackUserInteractions: true,
+  trackResources: true,
+  trackLongTasks: true,
+  defaultPrivacyLevel: 'mask-user-input',
+});
 
 
 
 
 export default function Home() {
   const [current, setCurrent] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      // Track page view on route change
+      datadogRum.startView(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <PageBox>
@@ -27,15 +55,6 @@ export default function Home() {
 
 
       </Head>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-ZW9CXBGHF2"></Script>
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments)}
-            gtag('js', new Date());
-            gtag('config', 'G-ZW9CXBGHF2');
-          `}
-      </Script>
       <NavBar current={current} />
       <HomeSection1 current={current} setCurrent={setCurrent} />
       <HomeSection2 current={current} setCurrent={setCurrent} />
